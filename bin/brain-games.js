09f5555fecd80862ae66cc5759e30ba42ca7870c
Ali <aliumarov90@mail.ru>
2023-02-06 20:01:47 +0300

    update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
-import { greeting } from '../src/index.js';
+import readlineSync from 'readline-sync';
 
-greeting();
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello ${name}`);
